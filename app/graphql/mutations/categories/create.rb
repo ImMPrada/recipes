145bfede149a86d7ce3abf341f2f9786914f53d1
immprada <im.mprada@gmail.com>
2025-03-23 14:23:07 -0500

    feat: add create category mutation

---
+++ app/graphql/mutations/categories/create.rb
@@ -0,0 +1,15 @@
+module Mutations
+  module Categories
+    class Create < Mutations::BaseMutation
+      argument :name, String, required: true
+
+      field :category, Types::App::CategoryType, null: false
+
+      def resolve(name:)
+        category = ::Category.find_or_create_by(name: name)
+
+        { category: category }
+      end
+    end
+  end
+end
