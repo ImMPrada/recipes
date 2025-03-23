b29ee47a7f28c2b97800f4a92507b77449063478
immprada <im.mprada@gmail.com>
2025-03-23 14:23:10 -0500

    feat: add delete category mutation

---
+++ app/graphql/mutations/categories/delete.rb
@@ -0,0 +1,20 @@
+module Mutations
+  module Categories
+    class Delete < Mutations::BaseMutation
+      argument :id, ID, required: true
+
+      field :message, String, null: false
+
+      def resolve(id:)
+        begin
+          category = ::Category.find(id)
+          category.destroy
+
+          { message: "Category deleted" }
+        rescue ActiveRecord::RecordNotFound
+          { message: "Category not found" }
+        end
+      end
+    end
+  end
+end
