f3bfae681b881780e0ba4007da3e4c9e21409c4b
immprada <im.mprada@gmail.com>
2025-03-22 19:08:14 -0500

    Update seeds with sample data for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+Category.destroy_all
+
+Category.create(name: "Breakfast")
+Category.create(name: "Lunch")
+Category.create(name: "Dinner")
+Category.create(name: "Snack")
+Category.create(name: "Dessert")
+Category.create(name: "Cocktail")
