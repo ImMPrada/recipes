37c39107baea198419fc7422948258eb5cdd089e
immprada <im.mprada@gmail.com>
2025-03-22 19:34:09 -0500

    feat: update seed data for recipes

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,31 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-
+puts "\e[33m destroying all \e[0m"
 Category.destroy_all
+Recipe.destroy_all
+puts "\e[32m done! \e[0m"
 
-Category.create(name: "Breakfast")
+
+
+puts "\e[33m creating categories \e[0m"
+breakfast_category = Category.create(name: "Breakfast")
 Category.create(name: "Lunch")
 Category.create(name: "Dinner")
 Category.create(name: "Snack")
 Category.create(name: "Dessert")
-Category.create(name: "Cocktail")
+cocktail_category = Category.create(name: "Cocktail")
+puts "\e[32m done! \e[0m"
+
+puts "\e[33m creating recipes \e[0m"
+Recipe.create(
+  name: "Eggs Benedict",
+  category: breakfast_category,
+  description: "A classic breakfast dish with eggs, bacon, and toast."
+)
+Recipe.create(
+  name: "Boulevardier",
+  category: cocktail_category,
+  description: "A classic cocktail with bourbon, bitters, and orange bitters."
+)
+puts "\e[32m done! \e[0m"
